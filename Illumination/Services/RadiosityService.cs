5fd39627c0386f032aee0f55737ee38b7ed89e3d
Dmitry Orlov <dimaorol@gmail.com>
2023-05-03 14:44:53 +0300

    even more comments

--- Illumination/Services/RadiosityService.cs
+++ Illumination/Services/RadiosityService.cs
@@ -2,9 +2,18 @@
 
 namespace Illumination.Services;
 
+/// <summary>
+/// Conditions to exit the radiosity calculation cycle
+/// </summary>
 public class RadiosityExitCondition
 {
+    /// <summary>
+    /// Max number of emitting/reflecting steps
+    /// </summary>
     public int? Steps { get; }
+    /// <summary>
+    /// Max number of emitted + reflected flux by a single patch
+    /// </summary>
     public double? EpsFlux { get; }
 
     public RadiosityExitCondition(int? steps = null, double? epsFlux = null)
@@ -16,7 +25,7 @@ public class RadiosityExitCondition
     }
 
     /// <summary>
-    /// 
+    /// Is exiting the cycle at current step required?
     /// </summary>
     /// <param name="currentStep">In interval [0, +maxInt). 0 - first step</param>
     /// <param name="maxFlux"></param>
@@ -27,15 +36,37 @@ public class RadiosityExitCondition
             : maxFlux <= EpsFlux;
 }
 
+/// <summary>
+/// Radiosity values of the parch
+/// </summary>
 public class PatchValues
 {
+    /// <summary>
+    /// Flux to emit
+    /// </summary>
     public double Emitted { get; set; }
+    /// <summary>
+    /// Flux to reflect
+    /// </summary>
     public double Reflected { get; set; }
+    /// <summary>
+    /// Flux received and not reflected
+    /// </summary>
     public double Stored { get; set; }
 }
 
+/// <summary>
+/// Calculation of illumination though the radiosity method
+/// </summary>
 public static class RadiosityService
 {
+    /// <summary>
+    /// Calculation of illumination though the radiosity method for the given space
+    /// </summary>
+    /// <param name="space">Space for patches of which the radiosity is calculated</param>
+    /// <param name="exitCondition">Exit conditions of the algorithm</param>
+    /// <returns>List of radiosity values for for each patch at the end of each step</returns>
+    /// <exception cref="InvalidOperationException"></exception>
     public static List<Dictionary<Patch, PatchValues>> CalculateRadiosity(this Space space,
         RadiosityExitCondition exitCondition)
     {
@@ -77,6 +108,11 @@ public static class RadiosityService
         return overallPatchValues;
     }
 
+    /// <summary>
+    /// Sums up the radiosity values of all steps
+    /// </summary>
+    /// <param name="valuesList">Radiosity values at the end of each step</param>
+    /// <returns>Total radiosity values for for each patch</returns>
     public static Dictionary<Patch, PatchValues> SumRadiosity(this List<Dictionary<Patch, PatchValues>> valuesList)
     {
         var result = valuesList.First()
